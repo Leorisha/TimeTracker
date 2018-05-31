3626b63c4f84b5e7db5fc51de077b2ced918dedc
Ana Neto <a.neto@campus.fct.unl.pt>
2018-05-31 21:39:02 +0100

    Improve displayed data on HistoryInterfaceController to match previously saved sessions.

--- Time Tracker WatchKit Extension/HistoryInterfaceController.swift
+++ Time Tracker WatchKit Extension/HistoryInterfaceController.swift
@@ -17,13 +17,41 @@ class HistoryInterfaceController: WKInterfaceController {
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        table.setNumberOfRows(10, withRowType: "ClockRow")
+        guard let clockIns = UserDefaults.standard.array(forKey: "clockins") as? [Date], let clockOuts = UserDefaults.standard.array(forKey: "clockouts") as? [Date] else { return }
+        
+        setupTable(with: clockIns, and: clockOuts)
+    }
+    
+    func setupTable(with clockIns: [Date], and clockOuts: [Date]) {
+        
+        table.setNumberOfRows(clockIns.count, withRowType: "ClockRow")
         
         for i in 0...9 {
             if let row = table.rowController(at: i) as? ClockRow {
-                row.clockLabel.setText("\(i)")
+                let timeInterval = Int(clockOuts[i].timeIntervalSince(clockIns[i]))
+                row.clockLabel.setText(getTimeText(for: timeInterval))
             }
         }
+    }
+    
+    func getTimeText(for timeInterval: Int) -> String {
+        
+        var timeString = ""
+        
+        let hours = timeInterval / 3600
+        if hours > 0 {
+            timeString += "\(hours)h "
+        }
+        
+        let minutes = (timeInterval % 3600) / 60
+        if minutes > 0 {
+            timeString += "\(minutes)m "
+        }
+        
+        let seconds = timeInterval % 60
+        timeString += "\(seconds)s"
+        
+        return timeString
     }
 
 }
